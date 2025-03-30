2cd497055b23f0c1c50020adc206a4b3cb89d879
Harry Zhang <75111093+hairez@users.noreply.github.com>
2025-03-30 22:55:30 +0200

    Fix the bug
    Now the code converges for any choice of b1 b2

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 N = 100;
-dx = 1/(N-2);
-x = linspace(0,1,N-1);
+dx = 1/(N);
+x = linspace(dx, 1-dx, N-1);
 f = @(t) 1;
 
 function x = bj(b,t)
@@ -22,6 +22,7 @@ function g = grad(t,i)
         g = g(i);
 	else
 		g = [-1,-1];
+        %g = [0,0];
         g = g(i);
     end
 end
@@ -31,6 +32,9 @@ end
 B = @(b) arrayfun(@(t) bj(b,t),x); 
 dB = @(i) arrayfun(@(t) grad(t,i),x);
 F = arrayfun(@(t) f(t), x);
+
+%F(1) = 0;
+%F(end) = 0;
 
 % D matrix
 D = zeros(N-1,N-1);
@@ -47,27 +51,39 @@ D = D*(1/dx)^2;
 % U = (B(b)*D)\W;
 % bestäm b_i nummeriskt
 % bestäm B från (b1,b2)
-delta = 0.05;
+delta = 0.1;
 L = @(U,A,b) (transpose(F)*(U+A)-transpose(B(b)*D*U)*D*A);
 % B is symmetric
 %grad_L = transpose(DU)*dB(b)*D*A*dx;
 
 
 
-b = [0.5 0.4];
+b = [0.2 0.2];
+
+bb = B(b);
 
 err = 1;
 tol = 1e-4;
 while err > tol
     W = D\F'; 
 
-    U = (diag(B(b))*D)\W;
 
-    grad_L1 = (transpose(D*U)*diag(dB(1)))*D*U*dx;
-    grad_L2 = (transpose(D*U)*diag(dB(2)))*D*U*dx;
+    U = (diag(B(b))*D)\W;
+    %U(1) = 0;
+    %U(end) = 0;
+
+    grad_L1 = -(transpose(D*U)*diag(dB(1)))*D*U*dx;
+    grad_L2 = -(transpose(D*U)*diag(dB(2)))*D*U*dx;
     disp(["gradients", grad_L1,grad_L2, "b", b]);
 
     bnxt = b - delta*[grad_L1, grad_L2];
+
+    %bnxt(1) = max(bnxt(1), 1e-3);  % b_1 > 0
+    %bnxt(2) = max(bnxt(2), 1e-3);  % b_1 > 0
+    %if sum(bnxt) >= 1
+    %    bnxt = bnxt / (sum(bnxt) + 1e-3);  % scale to keep sum < 1
+    %end
+
     err = norm(bnxt - b);
     b = bnxt;
     %disp(b);
